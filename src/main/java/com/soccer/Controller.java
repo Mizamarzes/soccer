4c9f3838ccc9df05bc93f214de081933b4c088f3
mizamarzes <mizamarzes@gmail.com>
2024-06-23 15:23:29 -0500

    primer commit

---
+++ src/main/java/com/soccer/Controller.java
@@ -0,0 +1,11 @@
+package com.soccer;
+
+import java.util.Hashtable;
+
+import com.soccer.entity.Team;
+
+public class Controller {
+    public Hashtable <String, Team> equipos = new Hashtable<>();
+    
+
+}
---
+++ src/main/java/com/soccer/entity/Team.java
@@ -0,0 +1,62 @@
+package com.soccer.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private String nombre;
+    private String ciudad;
+    private List<Player> listJugadores;
+    private List<Coach> listEntrenadores;
+    private List<Doctor> listMasajistas;
+    
+    public Team() {
+        listEntrenadores = new ArrayList<Coach>();
+        listJugadores = new ArrayList<Player>();
+        listMasajistas = new ArrayList<Doctor>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public List<Player> getListJugadores() {
+        return listJugadores;
+    }
+
+    public void setListJugadores(Player player) {
+        this.listJugadores.add(player);
+    }
+
+    public List<Coach> getListEntrenadores() {
+        return listEntrenadores;
+    }
+
+    public void setListEntrenadores(Coach coach) {
+        this.listEntrenadores.add(coach);
+    }
+
+    public List<Doctor> getListMasajistas() {
+        return listMasajistas;
+    }
+
+    public void setListMasajistas(Doctor masajista) {
+        this.listMasajistas.add(masajista);
+    }
+
+    
+    
+
+}
