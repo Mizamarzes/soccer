e5c025b4b74afde3f74250679097bad07f233232
mizamarzes <mizamarzes@gmail.com>
2024-06-24 02:25:07 -0500

    feat: avances en viewTeam y Controller y main

--- src/main/java/com/soccer/Main.java
+++ src/main/java/com/soccer/Main.java
@@ -1,14 +1,67 @@
 package com.soccer;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.soccer.view.viewCoach;
+import com.soccer.view.viewDoctor;
+import com.soccer.view.viewPlayer;
 import com.soccer.view.viewTeam;
 
 public class Main {
     public static void main(String[] args) {
-        Controller ctrlTeams = new Controller();
-        viewTeam.controlador = ctrlTeams;
-        viewTeam vt = new viewTeam();
-        vt.start();
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.println("Jugador"+ eq.getLstJugadores().get(0).getNombre());
+        Controller ctrlTeams = new Controller();
+        // Controller ctrlPlayers = new Controller();
+        // Controller ctrlDoctor = new Controller();
+        // Controller ctrlCoach = new Controller();
+
+        viewTeam.controlador = ctrlTeams;
+        // viewPlayer.controlador = ctrlPlayers;
+        // viewDoctor.controlador = ctrlCoach;
+        // viewCoach.controlador = ctrlCoach;
+
+        viewTeam vt = new viewTeam();
+        // viewPlayer vp = new viewPlayer();
+        // viewDoctor vd = new viewDoctor();
+        // viewCoach vc = new viewCoach();
+
+         System.out.println("Menu de gestion de la liga betplay");
+        int opcion = 0;
+
+        while (true) {
+            System.out.println("Opciones: \n 1. Equipos \n 2. Jugadores \n 3. Masajistas \n 4. Entrenadores");
+            try {
+                opcion = sc.nextInt();
+                sc.nextLine(); 
+                if (opcion >= 1 && opcion <= 4) {
+                    break; 
+                } else {
+                    System.out.println("Opción no válida. Por favor, elija una opción entre 1 y 4.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Por favor, ingrese un número.");
+                sc.nextLine(); 
+            }
+        }
+
+        switch (opcion) {
+            case 1:
+                vt.start();
+                break;
+            case 2:
+                // vp.start();
+                break;
+            case 3:
+                // vd.start();
+                break;
+            case 4:
+                // vc.start();
+                break;
+        }
+
+        sc.close(); 
+
     }
 }
